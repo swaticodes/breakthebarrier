8eae1934c195bfa85eb51df451f9ea8125d3d11e
SeoHyun <>
2021-10-23 22:58:18 -0400

    Co-authored-by: swaticodes <swaticodes@users.noreply.github.com> Co-authored-by: leenbean424 <leenbean424@users.noreply.github.com> Co-authored-by: enz-cab <enz-cab@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -58,10 +58,27 @@ function App() {
 
     const [index, setIndex] = useState(0); // state variable, intialize index to be 0
     
+    
+    const SwitchButton = ({initialText, afterText}) => {
+        const [buttonText, setButtonText] = useState(initialText);
+
+        return (
+            <button className="text-blue-500" onClick={() => setButtonText(afterText)}>{buttonText}</button>
+        );
+    }
+
     //* set index to +1*/ 
     const incrementIndex = () => {
         setIndex((index + 1)% words.length)
     }
+    // function to set the text
+    // const [buttonText2, setButtonText2] = useState("Show english sound")
+    // const [buttonText3, setButtonText3] = useState("Show korean consonant")
+    // const [buttonText4, setButtonText4] = useState("Show korean word example")
+
+    // const changeText2 = (text) => setButtonText2(text)
+    // const changeText3 = (text) => setButtonText3(text)
+    // const changeText4 = (text) => setButtonText4(text)
 
     return (
         <>
@@ -71,18 +88,14 @@ function App() {
                 return <p>{word.english}</p>
             })
         } */}
-
-        <p className="text-red-300 text-5xl">{words[index]['english']}</p>
+        {/* <p className="text-red-300 text-5xl">{words[index]['english']}</p>
         <p className="text-blue-500 text-5xl">{words[index]['englishSyllable']}</p>
         <p className="text-green-700 text-5xl">{words[index]['korean']}</p>
-        {/* <p>' '</p>
-        <p>' '</p>
-        <p>' '</p> */}
-        <p className="text-indigo-300 text-5xl">{words[index]['koreanExample']}</p>
-        {/* <p>' '</p>
-        <p>' '</p>
-        <p>' '</p>
-        <p>' '</p> */}
+        <p className="text-indigo-300 text-5xl">{words[index]['koreanExample']}</p> */}
+        <SwitchButton initialText="Show English word" afterText={words[index]['english']}></SwitchButton>
+        <SwitchButton initialText="Show English sound from word" afterText={words[index]['englishSyllable']}></SwitchButton>
+        <SwitchButton initialText="Show Korean consonant" afterText={words[index]['korean']}></SwitchButton>
+        <SwitchButton initialText="Show Korean Example" afterText={words[index]['koreanExample']}></SwitchButton>
         <button className="bg-purple-500 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded" onClick={incrementIndex}>nextWord</button>
         </div>
         </>
