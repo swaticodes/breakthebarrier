56e1fc3a5948f7c7809abc2d8233f210067e2ef9
SeoHyun <>
2021-10-24 06:06:49 -0400

    projec

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {useState} from "react"
 import SwitchButton from "./components/SwitchButton"
 import {words} from "./data/words"
+import {audios} from "./data/audios"
+import React, { Component } from "react"
+import ReactHowler from 'react-howler'
 function App() {
 
     //create a function to:
@@ -13,7 +16,7 @@ function App() {
     //words[0]['english']
     //delay appearance using CSS
     //JSX - no components or state or hooks
-    //can call functions in HTML
+    //can call functions in HTMLnp
     //.map() js - array.map() x -> y
     //buttons- react on click
 
@@ -23,47 +26,63 @@ function App() {
     //translate to move
     //opacity to fade in + colors
     //transition duration- defines how long transition takes and spacing out transition
-
+    let audios = [
+        'https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-eo.mp3',
+        'https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-a.mp3',
+        "https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-o.mp3",
+        "https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-yu.mp3",
+        "https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-ya.mp3"
+    ]
 
     const [index, setIndex] = useState(0); // state variable, intialize index to be 0
-    
-    
-    // const SwitchButton1 = ({initialText, afterText}) => {
-    //     const [buttonText, setButtonText] = useState(initialText);
-    //     return (
-    //         <button className="text-pink-200 text-3xl" onClick={() => setButtonText(afterText)}>{buttonText}</button>
-    //     );
-    // }
 
-    // const SwitchButton2 = ({initialText, afterText}) => {
-    //     const [buttonText, setButtonText] = useState(initialText);
-
-    //     return (
-    //         <button className="text-purple-200 text-3xl" onClick={() => setButtonText(afterText)}>{buttonText}</button>
-    //     );
-    // }
-
-    // const SwitchButton3 = ({initialText, afterText}) => {
-    //     const [buttonText, setButtonText] = useState(initialText);
-
-    //     return (
-    //         <button className="text-blue-300 text-3xl" onClick={() => setButtonText(afterText)}>{buttonText}</button>
-    //     );
-    // }
-
-    // const SwitchButton4 = ({initialText, afterText}) => {
-    //     const [buttonText, setButtonText] = useState(initialText);
-
-    //     return (
-    //         <button className="text-green-200 text-3xl" onClick={() => setButtonText(afterText)}>{buttonText}</button>
-    //     );
-    // }
-
-    //* set index to +1*/ 
     const incrementIndex = () => {
         setIndex((index + 1)% words.length)
         console.log(words[index])
     }
+    
+    const playSound = () => {
+        return (
+            <ReactHowler src = {audios[index]} playing = {true}/>
+        )
+    }
+    
+    
+    const SwitchButton1 = ({initialText, afterText}) => {
+        const [buttonText, setButtonText] = useState(initialText);
+        return (
+            <button className="text-pink-200 text-3xl" onClick={() => setButtonText(afterText)}>{buttonText}</button>
+        );
+    }
+
+    const SwitchButton2 = ({initialText, afterText}) => {
+        const [buttonText, setButtonText] = useState(initialText);
+
+        return (
+            <button className="text-purple-200 text-3xl" onClick={() => setButtonText(afterText)}>{buttonText}</button>
+        );
+    }
+
+    const SwitchButton3 = ({initialText, afterText}) => {
+        const [buttonText, setButtonText] = useState(initialText);
+        const buttonEvents = () => {
+            setButtonText(afterText)
+        }
+
+        return (
+            <button className="text-blue-300 text-3xl" onClick={() => buttonEvents()}>{buttonText}</button>
+        );
+    }
+
+    const SwitchButton4 = ({initialText, afterText}) => {
+        const [buttonText, setButtonText] = useState(initialText);
+
+        return (
+            <button className="text-green-200 text-3xl" onClick={() => setButtonText(afterText)}>{buttonText}</button>
+        );
+    }
+
+    //* set index to +1*/ 
     // function to set the text
     // const [buttonText2, setButtonText2] = useState("Show english sound")
     // const [buttonText3, setButtonText3] = useState("Show korean consonant")
@@ -75,6 +94,7 @@ function App() {
 
     return (
         <>
+        <ReactHowler src = {audios[index]} playing = {true}/>np
         <div className="w-full h-screen bg-gray-900 flex flex-col items-center justify-evenly"> 
         {/* {
             words.map((word) => {
@@ -85,10 +105,10 @@ function App() {
         <p className="text-blue-500 text-5xl">{words[index]['englishSyllable']}</p>
         <p className="text-green-700 text-5xl">{words[index]['korean']}</p> 
         <p className="text-indigo-300 text-5xl">{words[index]['koreanExample']}</p> */}
-        <SwitchButton initialText="Show English word" afterText={words[index]['english']} color="text-pink-200" >Show English word</SwitchButton>
-        <SwitchButton initialText="Show English sound from word" afterText={words[index]['englishSyllable']} color="text-purple-200">Show English sound from word</SwitchButton>
-        <SwitchButton initialText="Show Korean consonant" afterText={words[index]['korean']} color="text-blue-300">Show Korean consonant</SwitchButton>
-        <SwitchButton initialText="Show Korean Example" afterText={words[index]['koreanExample']} color="text-green-200">Show Korean example</SwitchButton>
+        <SwitchButton1 initialText="Show English word" afterText={words[index]['english']} color="text-pink-200" >Show English word</SwitchButton1>
+        <SwitchButton2 initialText="Show English sound from word" afterText={words[index]['englishSyllable']} color="text-purple-200">Show English sound from word</SwitchButton2>
+        <SwitchButton3 initialText="Show Korean consonant" afterText={words[index]['korean']} color="text-blue-300">Show Korean consonant</SwitchButton3>
+        <SwitchButton4 initialText="Show Korean Example" afterText={words[index]['koreanExample']} color="text-green-200">Show Korean example</SwitchButton4>
         <button className="bg-indigo-500 hover:bg-indigo-300 text-white font-bold py-2 px-4 rounded text3xl animate-bounce" onClick={incrementIndex}>Next Word</button>
         </div>
         </>
---
+++ src/data/audios.js
@@ -0,0 +1,8 @@
+export const audios = [
+    'https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-eo.mp3',
+    'https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-a.mp3',
+    "https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-o.mp3",
+    "https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-yu.mp3",
+    "https://d27hu3tsvatwlt.cloudfront.net/mfsource/kr/main/alpha_m/kr-m-zy-ya.mp3"
+
+]
